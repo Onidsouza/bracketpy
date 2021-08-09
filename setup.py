a40405a0dbff34c655864df913b434646fef19d3
Onidsouza <henrique.ams.souza@gmail.com>
2021-08-09 17:55:55 -0300

    added first files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='bracketpy',
+      version='0.0.1',
+      description='Domain-agnostic implementation of finite dimensional Lie '
+                  'algebras and related algorithms',
+      url='https://github.com/Onidsouza/bracketpy',
+      author='Henrique Souza',
+      author_email='henrique.ams.souza@gmail.com',
+      license='GNU General Public License v3.0',
+      packages=['bracketpy'],
+      zip_safe=False)
